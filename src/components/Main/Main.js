135d76177facf1d76de08db1f128508e991ed68d
jennkim0210 <dohakimm0210@gmail.com>
2024-08-07 19:17:10 +0900

    merge commit

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -25,9 +25,36 @@ const Main = () => {
   } = useContext(Context);
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [chatType, setChatType] = useState('');
+  const [chatType, setChatType] = useState("");
   const [questionIdx, setQuestionIdx] = useState(0);
-  const testQuestion = ['1. 테스트 질문입니다.', '2. 테스트 질문입니다.', '3. 테스트 질문입니다.']
+  const [showRankResult, setShowRankResult] = useState([]);
+  const testQuestion = [
+    "1. 테스트 질문입니다.",
+    "2. 테스트 질문입니다.",
+    "3. 테스트 질문입니다.",
+  ];
+
+  let result = {
+    ENTP: 0,
+    INTP: 0,
+    ENTJ: 0,
+    INTJ: 0,
+    ENFP: 0,
+    INFP: 0,
+    ENFJ: 0,
+    INFJ: 0,
+    ESTP: 0,
+    ISTP: 0,
+    ESTJ: 0,
+    ISTJ: 0,
+    ESFP: 0,
+    ISFP: 0,
+    ESFJ: 0,
+    ISFJ: 0,
+  };
+
+  let rankResult = [];
+
   const openModal = () => {
     setModalIsOpen(true);
   };
@@ -38,37 +65,54 @@ const Main = () => {
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      if(chatType==='mbti')clickInput(input, "mbti");
+      if (chatType === "mbti") clickInput(input, "mbti");
       else onSent();
     }
   };
 
-  const clickInput = (text="", chatType="") => {
-    if (chatType==='mbti') {
-      if(text.length > 0){
-        setMessage((prevMessage) => [...prevMessage, {'type':'title', 'context':text}]);
+  const clickInput = (text = "", chatType = "") => {
+    if (chatType === "mbti") {
+      if (text.length > 0) {
+        setMessage((prevMessage) => [
+          ...prevMessage,
+          { type: "title", context: text },
+        ]);
         setInput("");
       }
-      setChatType(chatType)
+      setChatType(chatType);
       // console.log("API연결");
       // const fetchData = async () => {
       //   const res = await axios.post("http://127.0.0.1:7080/");
       //   return res.data;
       // };
       // fetchData().then((res) => console.log("API 결과: ", res));
-      setMessage((prevMessage) => [...prevMessage, {'type':'mbti', 'context':testQuestion[questionIdx%3]}]);
-      setShowResult(true)
-      setQuestionIdx(questionIdx+1)
-      const response = {'ENTJ': 0.28, 'ENFJ': 0.22, 'INFP': 0.18}
+      setMessage((prevMessage) => [
+        ...prevMessage,
+        { type: "mbti", context: testQuestion[questionIdx % 3] },
+      ]);
+      setShowResult(true);
+      setQuestionIdx(questionIdx + 1);
+      const response = { ENTJ: 0.28, ENFJ: 0.22, INFP: 0.18 };
+
+      for (let key in response) {
+        if (response.hasOwnProperty(key) && result.hasOwnProperty(key)) {
+          result[key] += response[key];
+        }
+      }
+
+      let sortedresult = Object.entries(result).sort((a, b) => b[1] - a[1]);
+      setShowRankResult(sortedresult.slice(0, 3));
+
+      console.log(rankResult);
     } else {
-      setChatType('')
+      setChatType("");
       setInput(text);
       onSent(text);
     }
   };
 
   const home = () => {
-    if(!loading){
+    if (!loading) {
       setShowResult(false);
       setMessage("");
       setChatType("");
@@ -96,6 +140,7 @@ const Main = () => {
             resultData={resultData}
             message={message}
             loading={loading}
+            showRankResult={showRankResult}
           />
         )}
 
